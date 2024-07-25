1fdc415218b0bfbbd268a8efbe17a4ddfe8ece10
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2024-07-25 23:18:41 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+ /**
+ * Auth Routes
+ *
+ * These routes handle user authentication, including login, registration, and logout.
+*/
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+
+/**
+ * Authenticated user Routes
+ *
+ * These routes handle Auth user  operations.
+ */
+Route::middleware('auth:sanctum')->group(function () {
+
+});
+
+
+/**
+ * Admin Management Routes
+ *
+ * These routes handle Admin management operations.
+ */
+Route::middleware(['auth', 'admin'])->group(function () {
+
+});
+
+
